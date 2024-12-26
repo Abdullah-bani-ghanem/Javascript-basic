75dcf42d1fd6698edef60e4955b40b20bc6468f4
Your Name <abdullahbanighanem@gmile.com>
2024-12-26 16:14:53 +0300

    javadcrept

---
+++ index.js
@@ -0,0 +1,59 @@
+console.log(5*3);
+console.log("JavaScript" + 50);
+console.log(17 % 5);
+console.log(5 % 17);
+console.log(5 / 10);
+console.log(4==='4');
+console.log(4!=5);
+console.log(7<=8);
+console.log("Hello"+5);
+console.log(Math.ceil(5)-Math.floor(10.5));
+console.log(Math.pow(4,2));
+
+
+
+
+
+var x=window.prompt("enter number");
+alert(x);
+
+
+
+
+
+
+
+
+var s=window.prompt( "enter first number");
+var m=window.prompt( "enter sceond number");
+alert(+s + +m);
+
+
+
+
+
+
+
+
+
+var u=window.prompt( "enter first number");
+var j=window.prompt( "enter sceond number");
+alert(+u * +j);
+
+
+
+
+
+
+
+
+
+var r=window.prompt( "enter first number");
+var e=window.prompt( "enter sceond number");
+alert(+r / +e);
+
+
+
+
+
+alert(5+5);
